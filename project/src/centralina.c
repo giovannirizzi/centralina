adde79657d4701c7e045ffe2fafa06bebc524bc0
Giovanni RIzzi <giovannirizzi98@live.com>
2019-04-22 18:44:18 +0200

    Aggiunto file TODO, esempio di codice per il parsing dei comandi e devices.h

--- project/src/centralina.c
+++ project/src/centralina.c
@@ -1,10 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "centralina.h"
 
 
-int main(){
-    
-    printf(TEST);
-    return 0;
+int main(int argc, char *argv[]){
+
+    //ESEMPIO DI CODICE PER IMPLEMENTARE IL PARSING DEI COMANDI
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    //Legge una riga finche non si verifica un errore 
+    while ((nread = getline(&line, &len, stdin)) != -1) {
+        //Sostituisce l'ultimo carattere della riga letta \n con \0
+        if(nread != -1)
+            line[nread-1] = '\0';
+
+        printf("line: %s\n", line);
+
+        char *tok = line, *end = line;
+        while (tok != NULL) {
+            //Sostituisce nella linea letta il primo " " che incontra con \0
+            strsep(&end, " ");
+            printf("Step: %s\n",tok);
+            tok = end;
+        }
+        
+    }
+
+    free(line);
+    exit(EXIT_SUCCESS);
 }
--- project/src/centralina.h
+++ project/src/centralina.h
@@ -1,1 +1,12 @@
-#define TEST "swag"
+#ifndef CENTRALINA_H
+#define CENTRALINA_H
+
+#include "devices.h"
+
+void list();
+int add_device(DEVICE_NAME device);
+int delete_device(DEVICE_ID device);
+int switch_device(DEVICE_ID device1, DEVICE_ID device2);
+int get_info(DEVICE_ID device);
+
+#endif // CENTRALINA_H
