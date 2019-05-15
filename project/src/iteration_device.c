f302843dc87e1136da14ef35a1dc3df6e8bf9a23
Omar Battan <omarbattan@gmail.com>
2019-05-15 14:57:07 +0200

    iteration device senza controlli

--- project/src/iteration_device.c
+++ project/src/iteration_device.c
@@ -19,11 +19,43 @@ int handle_device_command(const Command *c, const CommandBind extra_commands[], 
 }*/
 
 void setconf_command(const char** args, const size_t n_args){
-    sscanf(args[0], "%d-%d-%d", &g_device.id, &g_device.type, &g_device.state);
+    char* records[1], *registry[10], *value_str[1];
+    int i, nRegistry, value;
+    divide_string((char*)*args, records, 1, "$");
+    sscanf(args[0], "%d|%d|%d", &g_device.id, &g_device.type, &g_device.state);
+    nRegistry = divide_string(records[0], registry, 10, "&");
+
+    divide_string(records[0], value_str, 1, "=");
+    string_to_int(value_str[0], &value);
+    for(i=0; i<g_device.num_records; i++)
+        if(strcmp(g_device.records[i].label, records[0])==0) 
+            g_device.records[i].value = value;
+
+    for(i=0; i<nRegistry; i++){
+        divide_string(registry[i], value_str, 1, "=");
+        string_to_int(value_str[0], &value);
+        for(i=0; i<g_device.num_records; i++)
+            if(strcmp(g_device.records[i].label, registry[i])==0) 
+                g_device.records[i].value = value; 
+    }    
+
 }
 
 void getconf_command(const char** args, const size_t n_args){
-    info_command(NULL, 0);
+    print_error("Device %d: received getconf command\n", g_device.id);
+    char info_string[200], tmp[50];
+    sprintf(info_string, "%d|%d|%d", g_device.id, g_device.type, g_device.state);
+    int i;
+    for(i=0; i<g_device.num_records; i++){
+        if(i==0)
+            sprintf(tmp, "$%s=%d", g_device.records[i].label, g_device.records[i].value);
+        else
+            sprintf(tmp, "&%s=%d", g_device.records[i].label, g_device.records[i].value);
+        
+        strcat(info_string, tmp);
+    }
+
+    fprintf(g_curr_out_stream, "%s\n", info_string);
 }
 
 void switch_command(const char** args, const size_t n_args){
