b9ffa82f328db9ea3a146fad12b006b12fdac707
Omar Battan <omarbattan@gmail.com>
2019-05-20 11:52:49 +0200

    timer pregiato

--- project/src/timer.c
+++ project/src/timer.c
@@ -4,24 +4,20 @@
 #include "control_device.h"
 
 void switch_power_action(int state);
-void tick_signal(int a);
-
-timer_t timer;
 
 int main(int argc, char *argv[]){
 
     Registry records[] = {
-            {"begin", "starting time", 0, &string_to_int, &seconds_to_string, false}
+            {"begin", "starting time", 0, &string_to_int, &seconds_to_string, false},
             {"end", "ending time", 0, &string_to_int, &seconds_to_string, false}
     };
     Switch switches[] = {"power", &switch_power_action};
 
     SignalBind signal_bindings[] = {
-            {SIG_POWER, &switch_power_action},
-            {SIG_TICK, &tick_signal}
+            {SIG_POWER, &switch_power_action}
     };
 
-    DeviceData bulb = {
+    DeviceData timer = {
             TIMER, //DEVICE TYPE
             -1, //ID
             0, //STATE
@@ -32,7 +28,6 @@ int main(int argc, char *argv[]){
     };
 
     g_device = timer;
-    create_timer(&timer);
 
     init_base_device(argv, argc);
     
@@ -40,22 +35,12 @@ int main(int argc, char *argv[]){
             NULL, 0);
 
 
-    delete_timer(&timer);
     clean_base_device();
 
     exit(EXIT_SUCCESS);
 }
 
-void tick_signal(const int a){
-    g_device.records[0].value++;
-}
-
 void switch_power_action(int state){
 
-    if(state==0){
-        set_timer_tick(timer, false);
-        g_device.records[0].value = 0;
-    } else
-        set_timer_tick(timer, true);
     g_device.state = state;
 }
